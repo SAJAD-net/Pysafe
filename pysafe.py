de5f7a99d2e609ee30dc23a5e8f6a3b4d4fe6f94
SAJADnet <sajadsajad.sch@gmail.com>
2023-02-21 08:35:45 -0500

    passed first test

--- pysafe.py
+++ pysafe.py
@@ -5,6 +5,7 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 
 import secrets
+import sys
 import base64
 import getpass
 
@@ -15,14 +16,15 @@ def generate_salt(size=16):
 
 def derive_key(salt, password):
     """Derive the key from the `password` using the passed `salt`"""
-    kdf = Scrypt(salt=salt, length=32, n=2**20, r=8, p=1)
+    kdf = Scrypt(salt=salt, length=32, n=2**14, r=8, p=1)
     return kdf.derive(password.encode())
 
 def load_salt():
     # load salt from pysalt.salt file
     return open("pysalt.salt", "rb").read()
 
-def generate_key(password, salt_size=16, load_salt=False, save_salt=True):
+
+def generate_key(password, salt_size=16, load_existing_salt=False, save_salt=True):
     """
     Generates a key from a `password` and the salt.
     If `load_salt` is True, it'll load the salt from a file
@@ -30,7 +32,7 @@ def generate_key(password, salt_size=16, load_salt=False, save_salt=True):
     If `save_salt` is True, then it will generate a new salt
     and save it to "pysalt.salt"
     """
-    if load_salt:
+    if load_existing_salt:
         # load existing salt
         salt = load_salt()
     elif save_salt:
@@ -42,6 +44,8 @@ def generate_key(password, salt_size=16, load_salt=False, save_salt=True):
     derived_key = derive_key(salt, password)
     # encode it using Base 64 and return it
     return base64.urlsafe_b64encode(derived_key)
+
+
 
 def encrypt(filename, key):
     """
@@ -56,61 +60,75 @@ def encrypt(filename, key):
     # write the encrypted file
     with open(filename, "wb") as file:
         file.write(encrypted_data)
+    print("[+] File encrypted successfully")
+
 
 def decrypt(filename, key):
-    """
-    Given a filename (str) and key (bytes), it decrypts the file and write it
-    """
-    f = Fernet(key)
-    with open(filename, "rb") as file:
-        # read the encrypted data
-        encrypted_data = file.read()
-    # decrypt data
-    try:
-        decrypted_data = f.decrypt(encrypted_data)
-    except cryptography.fernet.InvalidToken:
-        print("[!] Invalid token, most likely the password is incorrect")
-        return
-    # write the original file
-    with open(filename, "wb") as file:
-        file.write(decrypted_data)
-    print("[+] File decrypted successfully")
+	"""
+	Given a filename (str) and key (bytes), it decrypts the file and write it
+	"""
+	f = Fernet(key)
+	with open(filename, "rb") as file:
+	    # read the encrypted data
+	    encrypted_data = file.read()
+	# decrypt data
+	try:
+	    decrypted_data = f.decrypt(encrypted_data)
+	#except cryptography.fernet.InvalidToken:
+	except Exception as err:
+		print(err)
+		print("[!] Invalid token, most likely the password is incorrect")
+		return
+	# write the original file
+	with open(filename, "wb") as file:
+		file.write(decrypted_data)
+	print("[+] File decrypted successfully")
+
+
+
+def pass_for_keygen():
+	password = getpass.getpass("[+] Enter the password for encryption: ")
 
 
 if __name__ == "__main__":
-    import argparse
-    
-    parser = argparse.ArgumentParser(description="PySafe File Encryptor")
-    parser.add_argument("-f", "--file", help="File to encrypt/decrypt")
-    parser.add_argument("-s", "--salt-size", help="If this is set, a new salt with the passed size is generated",
-                        type=int)
-    parser.add_argument("-e", "--encrypt", action="store_true",
-                        help="Whether to encrypt the file, only -e or -d can be specified.")
-    parser.add_argument("-d", "--decrypt", action="store_true",
-                        help="Whether to decrypt the file, only -e or -d can be specified.")
+	import argparse
 
-    args = parser.parse_args()
-    file = args.file
+	parser = argparse.ArgumentParser(description="PySafe File Encryptor")
+	parser.add_argument("-f", "--file", help="File to encrypt/decrypt")
+	parser.add_argument("-s", "--salt-size",required=False, help="If this is set, a new salt with the passed size is generated",
+	                    type=int)
+	parser.add_argument("-e", "--encrypt", action="store_true",
+	                    help="Whether to encrypt the file, only -e or -d can be specified.")
+	parser.add_argument("-d", "--decrypt", action="store_true",
+	                    help="Whether to decrypt the file, only -e or -d can be specified.")
 
-    if args.encrypt:
-        password = getpass.getpass("[+] Enter the password for encryption: ")
-    elif args.decrypt:
-        password = getpass.getpass("[+] Enter the password you used for encryption: ")
+	args = parser.parse_args()
+	file = args.file
+	try:
 
-    if args.salt_size:
-        key = generate_key(password, salt_size=args.salt_size, save_salt=True)
-    else:
-        key = generate_key(password, load_salt=True)
+		if args.encrypt:
+			password = getpass.getpass("[+] Enter the password for encryption: ")
+		elif args.decrypt:
+			password = getpass.getpass("[+] Enter the password you used for encryption: ")
 
-    encrypt_ = args.encrypt
-    decrypt_ = args.decrypt
+		if args.salt_size:
+			key = generate_key(password, salt_size=args.salt_size, save_salt=True)
+		else:
+			key = generate_key(password, load_existing_salt=True)
 
-    war_message = "[!] Please specify whether you want to encrypt the file or decrypt it."
-    if encrypt_ and decrypt_:
-        raise TypeError(war_message)
-    elif encrypt_:
-        encrypt(file, key)
-    elif decrypt_:
-        decrypt(file, key)
-    else:
-        raise TypeError(war_message)
+	except Exception as err:
+		parser.print_help()
+		sys.exit()
+
+	encrypt_ = args.encrypt
+	decrypt_ = args.decrypt
+
+	war_message = "[!] Please specify whether you want to encrypt the file or decrypt it."
+	if encrypt_ and decrypt_:
+	    raise TypeError(war_message)
+	elif encrypt_:
+	    encrypt(file, key)
+	elif decrypt_:
+	    decrypt(file, key)
+	else:
+	    raise TypeError(war_message)
